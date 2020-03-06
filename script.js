74d4400dfa74093b12ee93f70e9585898a8abd93
zul'jin <53787083+6mahdapihka9@users.noreply.github.com>
2020-03-06 14:00:50 +0200

    js update

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-funcction few(){
-    
-}
+let res = (a)=>{
+    let sum = a;
+    function add(b) {
+        if (b) {
+            sum += b;
+            return sum;
+        }
+    }
+    return a;
+};
+
+const add = (a) => {
+    let sum = a;
+    const func = (b) => {
+        if (b) {
+            sum += b;
+            return func;
+        } else {
+            return sum;
+        }
+    };
+    return func;
+};
+
+console.log( add(10)(5)(2) );
