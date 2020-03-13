8a4f3f8015537933c11a5e667fc22a44f9463b5e
zul'jin <53787083+6mahdapihka9@users.noreply.github.com>
2020-03-13 18:16:50 +0200

    js update: a lot of methods that work with forms. html update: form updated. it works!!!

--- script.js
+++ script.js
@@ -1,32 +1,74 @@
 const CANVAS = document.getElementById('canvas');
 const CTX = CANVAS.getContext('2d');
-let cWidth = 800, cHeight = 650;
 let intersectionEachOthers = false;
-let oneCircle = true;
-let manyCircles = false;
-let atAllDots = true;
-let choosing = false;
-let dots = [];
+//let atAllDots = true;
+let d = [];
 let scaleRate = 1;
+let Cx, Cy, R,
+    tempX, tempY, tempR,
+    lenX, lenY,
+    smallR = 1;
+let inpX, inpY;
+let enough; //enough to draw circle
 let mouse = {
-    x : 0,
-    y : 0,
+    xPressed : 0,
+    yPressed : 0,
+    x: 0,
+    y: 0,
+    xReleased : 0,
+    yReleased : 0,
     down: false
 };
+
 CANVAS.onmousemove = function (e) {
     mouse.x = e.offsetX;
     mouse.y = e.offsetY;
 };
-CANVAS.onmousedown = function () {
+CANVAS.onmousedown = function (e) {
+    mouse.xPressed = e.offsetX;
+    mouse.yPressed = e.offsetY;
     mouse.down = true;
+    if (document.forms[0].inputByYourself.checked) {
+        let newDot = new Dot(mouse.x, mouse.y);
+        d.push( newDot );
+        /*
+        for (let i = 0; i < dots.length; i++)
+            for (let k in dots[i])
+                console.log( k  + " " + dots[i][k]);
+
+         */
+        CTX.strokeStyle = 'red';
+        CTX.beginPath();
+        CTX.arc(mouse.x, mouse.y, 1, 0, Math.PI*2, true);
+        CTX.stroke();
+    }
 };
-CANVAS.onmouseup = function () {
+CANVAS.onmouseup = function (e) {
+    mouse.xReleased = e.offsetX;
+    mouse.yReleased = e.offsetY;
     mouse.down = false;
 };
-
+class Dot{
+    constructor(_x, _y) {
+        if (_x !== undefined && _y !== undefined) {
+            this.x = _x;
+            this.y = _y;
+        } else {
+            this.x = 0;
+            this.y = 0;
+        }
+        this.r = 0;
+        this.diff = 0;
+        this.covered = false;
+    }
+    Diff (_x, _y, _R) {
+        this.r = Math.sqrt(Math.pow(this.x - _x, 2) + Math.pow(this.y - _y, 2));
+        this.diff = _R - this.r;
+    };
+}
 function zoom(in_out) {
     CTX.clearRect(0, 0, CANVAS.width, CANVAS.height);
-    CTX.fillStyle = "black";
+    CTX.fillStyle = "blue";
     CTX.beginPath();
     CTX.scale( (in_out)? scaleRate + 0.25 : scaleRate - 0.25, (in_out)? scaleRate + 0.25 : scaleRate - 0.25 );
     CTX.arc(100, 100, 100, 0, Math.PI*2, true);
@@ -37,12 +79,8 @@ function redraw() {
 
 }
 function grid(){
-    CTX.clearRect(0,0, CANVAS.width, CANVAS.height);
     CTX.beginPath();
-    CTX.moveTo(75,50);
-    CTX.lineTo(100,75);
-    CTX.lineTo(100,25);
-    CTX.stroke();
+    CTX.clearRect(0,0, CANVAS.width, CANVAS.height);
 }
 function dataImport(){
     //let coordinatesStr;
@@ -52,9 +90,18 @@ function dataExport(){
 
 }
 function clearAll() {
+    d = [];
     grid();
 }
 function built() {
+    grid();
+    if (d !== [])
+        for (let dIndex in d){
+            CTX.strokeStyle = 'red';
+            CTX.beginPath();
+            CTX.arc(d[dIndex].x, d[dIndex].y, 1, 0, Math.PI*2, true);
+            CTX.stroke();
+        }
     let form = document.forms[0];
     if (form.builtOne.checked)
         builtOne();
@@ -63,6 +110,66 @@ function built() {
 }
 function builtOne(){
     console.log("build one");
+    //two dots
+    let k = 0, m = 0, n = 0;
+    let max = 0, dist = [ d.length*(d.length-1)/2 ];
+
+    for (let i = 0; i < d.length-1; i++)
+    for (let j = i+1; j < d.length; j++) {
+        lenX = Math.abs(d[i].x - d[j].x);
+        lenY = Math.abs(d[i].y - d[j].y);
+        dist[k] = Math.sqrt(Math.pow(lenX, 2) + Math.pow(lenY,2));
+        if(max < dist[k]) {
+            max = dist[k];	m = i;	n = j;
+        }
+        k++;
+    }
+    tempX = (d[m].x + d[n].x)/2;
+    tempY = (d[m].y + d[n].y)/2;
+    tempR = Math.sqrt(Math.pow(d[m].x - tempX, 2) + Math.pow(d[m].y - tempY, 2));
+    enough = true;
+    for (let i = 0; i < d.length; i++)
+    if ((Math.pow(d[i].x-tempX, 2) + Math.pow(d[i].y-tempY, 2)) > Math.round(tempR*tempR * 100000000.0)/100000000.0 + 0.000000001)
+        //if ((Math.pow(d[i].x-tempX, 2) + Math.pow(d[i].y-tempY, 2)) > tempR*tempR)
+        enough = false;
+    Cx = tempX; Cy = tempY; R = tempR;
+
+    //three dots
+    if (!enough) {
+        let threeR = max, threeX = 0, threeY = 0;
+        console.log("beep");
+        for (let i = 0; i < d.length-2; i++)
+        for (let j = i+1; j < d.length-1; j++) {
+            for (let l = j+1; l < d.length; l++) {
+                Cx = -(d[i].y*(d[j].x*d[j].x + d[j].y*d[j].y - d[l].x*d[l].x - d[l].y*d[l].y) +
+                    d[j].y*(d[l].x*d[l].x + d[l].y*d[l].y - d[i].x*d[i].x - d[i].y*d[i].y) +
+                    d[l].y*(d[i].x*d[i].x + d[i].y*d[i].y - d[j].x*d[j].x - d[j].y*d[j].y))/
+                    (2*(d[i].x*(d[j].y-d[l].y) + d[j].x*(d[l].y - d[i].y) + d[l].x*(d[i].y - d[j].y)));
+                Cy = (d[i].x*(d[j].x*d[j].x + d[j].y*d[j].y - d[l].x*d[l].x - d[l].y*d[l].y) +
+                    d[j].x*(d[l].x*d[l].x + d[l].y*d[l].y - d[i].x*d[i].x - d[i].y*d[i].y) +
+                    d[l].x*(d[i].x*d[i].x + d[i].y*d[i].y - d[j].x*d[j].x - d[j].y*d[j].y))/
+                    (2*(d[i].x*(d[j].y-d[l].y) + d[j].x*(d[l].y - d[i].y) + d[l].x*(d[i].y - d[j].y)));
+                R = Math.sqrt( Math.pow(d[i].x - Cx, 2) + Math.pow(d[i].y - Cy, 2));
+
+                enough = true;
+                for (let iter = 0; iter < d.length; iter++)
+                //if ((Math.pow(d[iter].x-Cx, 2) + Math.pow(d[iter].y-Cy, 2)) > Math.round(R*R * 100000000.0)/100000000.0)
+                if ((Math.pow(d[iter].x-Cx, 2) + Math.pow(d[iter].y-Cy, 2)) > R*R)
+                    enough = false;
+                console.log("("+d[i].x+";"+d[i].y+") ("+d[j].x+";"+d[j].y+") ("+d[l].x+";"+d[l].y+") " + R + " " + enough);
+                if (enough && R < threeR) {
+                    threeX = Cx;	threeY = Cy;	threeR = R;
+                }
+            }
+        }
+
+        Cx = threeX;	Cy = threeY;	R = threeR;
+        console.log(R);
+    }
+    CTX.strokeStyle = 'blue';
+    CTX.beginPath();
+    CTX.arc(Cx, Cy, R, 0, Math.PI*2, true);
+    CTX.stroke();
 }
 function builtMany(){
     console.log("build many");
@@ -91,27 +198,5 @@ function able_disable() {
 function statsExport(){
 
 }
-class Dot{
-    constructor(_x, _y) {
-        if (_x !== undefined && _y !== undefined) {
-            this.x = _x;
-            this.y = _y;
-        } else {
-            this.x = 0;
-            this.y = 0;
-        }
-        this.r = 0;
-        this.diff = 0;
-        this.covered = false;
-    }
-    Diff (_x, _y, _R) {
-        this.r = Math.sqrt(Math.pow(this.x - _x, 2) + Math.pow(this.y - _y, 2));
-        this.diff = _R - this.r;
-    };
-}
 
-
-let dot = new Dot(5, 6);
-for (let k in dot)
-    console.log( k  + " " + dot[k]);
 
